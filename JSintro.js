34431c36ca6e6131abce73637383383bcee59fdd
mrityunjay9693 <mkaykumar123@gmail.com>
2023-01-24 00:24:36 +0530

    intro to js

---
+++ JSintro.js
@@ -0,0 +1,13 @@
+console.log("Hello World")      /* 
+                                    to terminate line use semi-colon or without semi-colon. Using semi-colon is a 
+                                    good pracice.  
+                                */
+console.log('Welcome to tilde.'); // We can write string data inside double quotation as well as single quotation
+
+/* 
+    Backtick(``) : Sometime it is called template literals or template strings which is anotherway to type out
+                   a string whithin ``. It is called backtick or grave accent.   
+                   You can also use backtick for using string inside it.
+                   We will see it later.
+*/
+console.log(`This is begining of JavaScript journey.`); 
