b322db20220eb1d096e277f9cad2cf700a23cd4f
mrityunjay9693 <mkaykumar123@gmail.com>
2023-01-26 15:33:42 +0530

    understanding "strict" in JS

---
+++ UseStrict.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+firstName = "Mrinal";   // error : firstName is not defined
+lastName = "Sauraj";    // error : lastName is not defined
+console.log(firstName, lastName);
+*/
+/*
+ *Using "strict" allows declaration of variable with keyword(var/let/const). 
+ *If we defined a variable without keyword(var/let/const) then there will be a error : variable is not defined.  
+ */
+let cityName = "Muzaffarpur,";
+let stateName = "Bihar";
+console.log("From",cityName,stateName); 
