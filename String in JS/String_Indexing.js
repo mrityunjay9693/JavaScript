601cfbf0a9c213b8a5fa8a908fad87ffcf07c6e0
mrityunjay9693 <mkaykumar123@gmail.com>
2023-01-28 09:54:47 +0530

    string indexing/position

---
+++ String in JS/String_Indexing.js
@@ -0,0 +1,12 @@
+/**
+ * String indexing: 
+   for example : gameName = Football;
+                 String(sequence of characters): F  o  o  t  b  a  l  l
+                                 index/position: 0  1  2  3  4  5  6  7
+*/
+let gameName = "Football";
+let pos = gameName[4];
+
+console.log(pos); // at index 4, 'b' is present
+
+console.log(gameName[1]);
