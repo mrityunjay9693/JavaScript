43fe32147b6545c71ee188c4b7737e84e5df5e3b
mrityunjay9693 <mkaykumar123@gmail.com>
2023-01-29 21:06:22 +0530

    trimStart() and trimEnd() also done

--- String in JS/trimMethod.js
+++ String in JS/trimMethod.js
@@ -2,6 +2,7 @@
 trim(): trim() method of string in JS is used to remove whitespaces(space characters) from both ends of the string and
         return a new string. It will not modify the existing(original) string. You have to either store modified string 
         in the new variable or you have to store modified string in the same variable as shown below
+        Syntax : variableName.trim()
         let name = "  Mrityunjay  ";
         name.trim(); 
         console.log(name); // "  Mrityunjay  "; //here, string store in name is not modified. 
@@ -22,3 +23,18 @@ console.log("String before trim:"+stateName);
 stateName = stateName.trim();
 console.log("String after trim:");
 console.log(stateName);
+
+/**
+ *If you want to remove whitespaces of given string from any one side. Then we have following two ways :
+  1.trimStart() : It removes whitespaces from begining of the given string and return new string.
+  2.trimEnd() : It removes whitespaces from end side of the given string and return new string. 
+ */
+//1.trimStart()
+let cityName = "  Muzaffarpur   ";
+cityName = cityName.trimStart();
+console.log(cityName);
+
+//1.trimEnd()
+let houseName = " Yadavas Villas  ";
+houseName = houseName.trimEnd();
+console.log(houseName);
