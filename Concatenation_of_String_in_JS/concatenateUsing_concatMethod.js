7036d68fa54be695dbfefa0794a9e67b11699e5b
mrityunjay9693 <mkaykumar123@gmail.com>
2023-02-08 21:00:03 +0530

    concatenate string using concat() method

--- Concatenation_of_String_in_JS/concatenateUsing_concatMethod.js
+++ Concatenation_of_String_in_JS/concatenateUsing_concatMethod.js
@@ -7,6 +7,19 @@ let str = "Good Morning";
 let greet = str.concat(", ", "Welcome to tilde.");
 console.log(greet);
 
+let firstName = "Mrityunjay";
+let middleName = "Kumar";
+let lastName = "Yadav";
+let myName = "My name is ".concat(
+  firstName,
+  " ",
+  middleName,
+  " ",
+  lastName,
+  "."
+);
+console.log(myName);
+
 //Ex2: We can also pass a variable as parameter which has string value to be concatenated with input string
 //and returns a new string.
 let hello = "Hello";
@@ -22,5 +35,5 @@ console.log(hello, world);
 let citiesName = ["Lucknow", "Kolkata", "Patna", "Bangalore", "Jaipur"];
 let output = "Indian Cities: ".concat(citiesName);
 let output1 = "Indian Cities: ".concat(...citiesName);
-console.log(output);    //Indian Cities: New Delhi,Kolkata,Patna,Bangalore,Jaipur
-console.log(output1);  //Spread operator is used-> Indian Cities: New DelhiKolkataPatnaBangaloreJaipur
+console.log(output); //Indian Cities: New Delhi,Kolkata,Patna,Bangalore,Jaipur
+console.log(output1); //Spread operator is used-> Indian Cities: New DelhiKolkataPatnaBangaloreJaipur
