025edb79dd37a2f4c9a8d0a612e5944929811c45
mrityunjay9693 <mkaykumar123@gmail.com>
2024-01-18 15:37:58 +0530

    Assignment operator program

---
+++ Expression and Operator  in JS/assignmentOperator.js
@@ -0,0 +1,13 @@
+/**
+ * Assignment operator: the symbol "=" is the assignment operator is used to assign the value in the variable operand to
+                        the left side of the operator
+                        eg: let x = 12; // 12 and x : operand , = is the assignment operator      
+ */
+//a]Simple assignment operator
+//in the below example, a numeric value is assigning to a variable operand. 
+let num = 65;
+console.log(num); 
+
+let myName = "Mrityunjay kumar";
+let myIntro = myName; //both are the variable(operands) either side of the assignment operator
+console.log(myIntro);
