1b277c1ffa4a4c9aaf00fdb0645e94bf1c0a1bb4
mrityunjay9693 <mkaykumar123@gmail.com>
2024-02-11 16:28:17 +0530

    length property

---
+++ Array in JS/lengthProperty.js
@@ -0,0 +1,13 @@
+/**
+ * Array Length : Array length represents the total number of elements stored/available within an array.
+ * length property is used to calculate the length of an array.
+ */
+//At first, length is calculated and stored in a variable and then print.
+let myCars = ["Audi", "Mercedes", "BMW", "Mahindra"];
+let lengthOfCars = myCars.length;
+console.log("Total number of cars: " + lengthOfCars);
+
+//directly using length property in console.log statement.
+let num = [10, 39, 578, 7, 0, 89];
+console.log("Total number of values: " + num.length);
+console.log(num.length);
