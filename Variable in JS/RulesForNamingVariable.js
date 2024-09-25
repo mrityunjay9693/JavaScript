58f90c718c2e8dc3f6abe99c7951451df8cda59c
mrityunjay9693 <mkaykumar123@gmail.com>
2024-09-25 22:01:48 +0530

    rules for naming  variable.

---
+++ Variable in JS/RulesForNamingVariable.js
@@ -0,0 +1,15 @@
+/**
+ * Rule 1: First Character: The first character of a variable name cannot be a digit. It must begin with a letter, 
+           dollar sign($), or underscore(_), but not a number.
+*/
+var validName = "Mrityunjay Kumar"
+console.log("My Name: " + validName)
+
+var $salary = 35000;
+console.log("My Salary: " + $salary);
+
+var _myRole = "Full Stack Developer";
+console.log("My Role: " + _myRole);
+
+//var 1value = 10;    // Invalid variable name : Cannot startv with digit
+//console.log("first value: " 1value);    //SyntaxError: Invalid or unexpected token
