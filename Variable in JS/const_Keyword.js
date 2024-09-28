2f78798fbc671bf1d70821ffbfe9cc0e82fa66e2
mrityunjay9693 <mkaykumar123@gmail.com>
2024-09-28 12:21:41 +0530

    updates.

--- Variable in JS/const_Keyword.js
+++ Variable in JS/const_Keyword.js
@@ -1,10 +1,10 @@
 /**
- * "const" is a keyword in JavaScript which is used to declaring a constant variable that will have a constant value 
-    throughout the program. Once a value declared to the variable with const keyword , its value will not be altered further.
+ * const: const is a keyword in JavaScript which is used to declaring a constant variable that will have a constant value 
+          throughout the program. Once a value declared to the variable with const keyword , its value will not be altered further.
  * Re-declaration and re-assigning of the variable declared with const keyword is not allowed here.
  * The "const" declaration creates a read-only reference to a value, and this reference cannot be changed.
  * Variable declared with 'const' keyword is a block-scope.
- * 'const' provides a level of safety and clarity.
+ * const provides a level of safety and clarity.
 */
 //Ex:1
 const pi = 3.14;
@@ -17,7 +17,11 @@ const countryName = "India";
 let stateName = "Bihar";
 let cityName = "Muzaffarpur";
 console.log(cityName,",", stateName,",", countryName);
-//If i want to change countryName which is declared using const, will not allow to change 
-//the value of variable countryName(declared using const)
-//countryName = "USA";
-//console.log(countryName); //error: re-assignment to constant variable not allowed 
+/**
+ * If i want to change countryName which is declared using const, will not allow to change 
+   the value of variable countryName(declared using const) 
+countryName = "USA";
+console.log(countryName); //error: re-assignment to constant variable not allowed 
+ */
+
+
