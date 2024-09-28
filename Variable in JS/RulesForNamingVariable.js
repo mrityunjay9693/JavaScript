e34c7478aba4f1714b169f8a1782e9c3390a4b09
mrityunjay9693 <mkaykumar123@gmail.com>
2024-09-28 10:40:50 +0530

    rule of naming variable.

--- Variable in JS/RulesForNamingVariable.js
+++ Variable in JS/RulesForNamingVariable.js
@@ -13,3 +13,22 @@ console.log("My Role: " + _myRole);
 
 //var 1value = 10;    // Invalid variable name : Cannot startv with digit
 //console.log("first value: " 1value);    //SyntaxError: Invalid or unexpected token
+
+/**
+ * Rule 2: Once the first character of a variable name is established(which must be a letter, $, _), as per the rule, 
+   the subsequent characters in the varibale name can include:
+   a)Letters : a - z, A - Z
+   b)Digits: 0 - 9
+   c)Underscopre character(_).
+   d)Dollar sign($).   
+ *  name cannot be a digit. It must begin with a letter, 
+           dollar sign($), or underscore(_), but not a number.
+*/
+
+//using letters and numbers 
+var name1 = "Sushma Swaraj";  // Valid : Digits are allowed after first character.
+var school_name = "Holy Mission SR SEC SCH";  //Valid : Special character underscore( _ ) is allowed after first character.
+var currency$ = "Dollar($)";   //Valid : Special character $ is allowed after first character.
+console.log("Name: "+name1);
+console.log("Schoole Name: "+school_name);
+console.log("Money accepted in: "+currency$);
