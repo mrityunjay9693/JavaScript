69a4a62b5f2415ec54f7eea7fc3f9e2b291d9337
mrityunjay9693 <mkaykumar123@gmail.com>
2024-09-28 14:00:37 +0530

    trim() updates.

--- String in JS/trimMethod.js
+++ String in JS/trimMethod.js
@@ -11,18 +11,42 @@ trim(): trim() method of string in JS is used to remove whitespaces(space charac
 */
 //1.Creating new variable
 let countryName = "  India  ";
-console.log("String before trim:"+countryName);
+console.log("String before trim:"+countryName); //Output:  India      
 console.log(countryName.length); // total number of characters : 9(length of string)
 let newName = countryName.trim(); // "India"
-console.log("String after trim:");
-console.log(newName);
+console.log("String after trim: ");
+console.log(newName); //OPutput:String after trim: India
+
+
+let subjectName = " JavaScript ";
+subjectName.trim(); //This returns the trimmed string but doesn't modify the original `subjectName` 
+console.log("After trim:"+subjectName); //After trim: JavaScript.
+
+//What is happening here : since strings are immutable in JavaScript, the original string subjectName is not 
+//modified after using trim() with subjectName to remove leading and trailing whitespaces.
+//The reason the spaces in subjectName are not removed after calling trim() in the following code is because 
+//the trim() method does not modify the original string. Instead, it returns a new string with the 
+//leading and trailing whitespace removed. In your code, you're not storing the result of the trim() operation.
+
+//Correct Approach: To fix this issue,we need to assign the result of subjectName.trim() 
+//either in same variable or in new one, like this:
+
+subjectName = subjectName.trim(); //Store the result of `trim()` in `subjectName` i.e., same variable
+console.log("Store the result of `trim()` in `subjectName`:"+subjectName); //"JavaScript"
+
+//using a new variable
+let newSubjectValue = subjectName.trim();
+console.log("In new variable:"+newSubjectValue); //"JavaScript"
+
+// Use the trim() method directly in console.log() to remove leading and trailing whitespace
+console.log(subjectName.trim());
 
 //2.Using same variable:
 let stateName = "  Bihar   ";
-console.log("String before trim:"+stateName);
+console.log("String before trim:"+stateName); //Output:String before trim:  Bihar   
 stateName = stateName.trim();
-console.log("String after trim:");
-console.log(stateName);
+console.log("String after trim:"); 
+console.log(stateName); ////Output: Bihar
 
 /**
  *If you want to remove whitespaces of given string from any one side. Then we have following two ways :
