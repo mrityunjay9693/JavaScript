b6fbcf06686417801e62ff9f9240c5f50901bda6
mrityunjay9693 <mkaykumar123@gmail.com>
2025-02-13 18:10:53 +0530

    getElementById

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,10 @@
-console.log(window.document);
+//console.dir(window.document);
+//Selecting an Element by using different methods of "document" object.
+//1. Selecting an Element using "get element by id" : It takes name of id of the element as input.
+console.log(document.getElementById("main-heading"));
+console.dir(document.getElementById("main-heading"));
+console.log(typeof document.getElementById("main-heading"));    //Return : object
+
+//Storing object in a variable declared using "const"
+const mainHeading = document.getElementById("main-heading");
+console.log(mainHeading); 
